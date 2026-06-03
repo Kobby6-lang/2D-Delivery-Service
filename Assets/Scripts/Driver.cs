ccef7dc5eadaf2f5f262e7d85a49e6e31d06760d
Kobby6-lang <koseitutu43@gmail.com>
2026-06-03 12:29:59 +0100

    Keyboard Testing Control

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,15 +1,31 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 0.5f;
     [SerializeField] float steerSpeed = .05f;
 
-
-    // Update is called once per frame
     void Update()
     {
+        if (Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("w is pressed");
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("s is pressed");
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("a is pressed");
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("d is pressed");
+        }
+
         transform.Translate(0, 0.05f, 0);
         transform.Rotate(0, 0, 0.5f);
     }
