be88edbbba3e255cb6c5f8abb45dbb793fe7d99b
Kobby6-lang <koseitutu43@gmail.com>
2026-06-10 19:57:02 +0100

    Finished with this

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 
-public class Clashing : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     bool hasDelivered;
+    [SerializeField] float delay = 1f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Delivery"))
+        if (collision.CompareTag("Delivery")&& !hasDelivered)
         {
             Debug.Log("Parcel");
             hasDelivered = true;
+            GetComponent<ParticleSystem>().Play();
+            Destroy(collision.gameObject, delay);
         }
         if (collision.CompareTag("Customer")&& hasDelivered)
         {
             Debug.Log("Delivered!");
             hasDelivered = false;
+            GetComponent<ParticleSystem>().Stop();
+            Destroy(collision.gameObject);
         }
 
     }
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,11 +1,16 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 0f;
-    [SerializeField] float steerSpeed = 0f;
+    [SerializeField] float currentSpeed = 10f;
+    [SerializeField] float steerSpeed = 200f;
+    [SerializeField] float boostSpeed = 15f;
+    [SerializeField] float regularSpeed = 5f;
+    [SerializeField] TMP_Text boostText;
     void Update()
     {
         float move = 0;
@@ -27,11 +32,34 @@ public class Driver : MonoBehaviour
         {
             steer = -1f;
         }
-        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float moveAmount = move * currentSpeed * Time.deltaTime;
         float steerAmount = steer * steerSpeed * Time.deltaTime;
 
         transform.Translate(0, moveAmount, 0);
         transform.Rotate(0, 0, steerAmount);
+
         
-    }      
+    }
+    void Start()
+    {
+        boostText.gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Boost"))
+        {
+            currentSpeed = boostSpeed;
+            boostText.gameObject.SetActive(true);
+            Destroy(collision.gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("WorldCollider"))
+        {
+            currentSpeed = regularSpeed;
+            boostText.gameObject.SetActive(false);
+        }
+        
+    }
 }
